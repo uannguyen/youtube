65ded1e149925367b394531aa3eeb9d216d00b10
nguyenconguan <uan.nguyen@lussoenterprise.com>
2021-08-14 20:56:42 +0700

    add basic file

---
+++ src/components/Header/header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = (props) => {
+
+  return (
+    <div className='header'>
+      <div className='container'>
+        <div className='header-logo' style={{ color: 'red' }}>
+          Hello
+        </div>
+        <div className='header-search'>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Layout/layout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Layout = (props) => {
+  return (
+    <div className=''>
+      
+    </div>
+  )
+}
+
+export default Layout
